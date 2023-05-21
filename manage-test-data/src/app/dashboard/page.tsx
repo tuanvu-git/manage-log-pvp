37a3c147d6b4afbadc1f32568787b6bdf41983fa
TUAN-VU\Admins <tuanvutran2112@gmail.com>
2023-05-21 22:31:18 +0700

    hidden text

--- manage-test-data/src/app/dashboard/page.tsx
+++ manage-test-data/src/app/dashboard/page.tsx
@@ -55,18 +55,18 @@ export default function Page({ params }: IPage) {
       let historyList: Report[] = res.data.history;
       let dateArr: any[] = [];
       for (let i = 2022; i < 2100; i++) {
-        dateArr.push(i);
+        dateArr.push(i.toString());
       }
       // historyList.sort(a,b) => {
       //   if(a.)
       // });
-      historyList.sort((a, b) => {
-        const sub = a.date.substring(0, 4);
-        if (dateArr.includes(sub)) {
-          return 0;
-        }
-        return -1;
-      });
+      // historyList.sort((a, b) => {
+      //   const sub = a.date.substring(0, 4);
+      //   if (dateArr.includes(sub)) {
+      //     return 0;
+      //   }
+      //   return -1;
+      // });
       const ticketReport = historyList.filter(r => {
         const sub = r.date.substring(0, 4);
         if (dateArr.includes(sub)) {
@@ -243,7 +243,7 @@ export default function Page({ params }: IPage) {
         <div className={`${isLoading ? 'flex' : 'hidden'} custom-spiner`}>
           <svg fill="none" viewBox="0 0 100 101" className="inline animate-spin text-gray-200 fill-blue-600 dark:text-gray-600 w-6 h-6 block root-spnier"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"></path><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"></path></svg>
         </div>
-        <h1 className={`${haveReportsToday ? "hidden" : "flex"} items-center justify-center text-red-400`}> Did you forget to insert the test data for {currentDay}? If not, please click
+        <h1 className={`${haveReportsToday ? "hidden" : "hidden"} items-center justify-center text-yellow-200`}> Did you forget to insert the test data for {currentDay}? If not, please click
           <Button className="ml-2" onClick={() => { setIsShowModel(true); dateRefModal.current!.value = currentDay; }}>
             here
           </Button>
