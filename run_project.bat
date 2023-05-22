184e27b5ab7004cf1040e08ed319a08cd047dace
TUAN-VU\Admins <tuanvutran2112@gmail.com>
2023-05-23 01:50:01 +0700

    reafcator v4

--- run_project.bat
+++ run_project.bat
@@ -5,10 +5,9 @@ for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
 cd ..
 del "*.db*"
 cd "%~dp0"
-start /b npm install 
+CALL npm install
 start /b npm run migrate-dev 
 start /b npm run dev
-
 start /b node server.js
 
 start /b npx prisma studio
